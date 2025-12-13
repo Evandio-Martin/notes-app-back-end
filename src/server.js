eeb27f009a4629607c076618cda4ce7bf3c34e94
Evandio <evandiomb@gmail.com>
2025-12-13 23:32:59 +0900

    restructure with hapi pluggin

--- src/server.js
+++ src/server.js
@@ -1,20 +1,27 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
-const server = Hapi.server({
-   port: 5000,
-   host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-   routes: {
-    cors: {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
         origin: ['*'],
+      },
     },
-   },
-});
+  });
 
-server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
-await server.start();
+  await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
